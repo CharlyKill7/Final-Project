d5d806f906d5f6a7e8d79910bb11c987d7fc2447
Carlos Mato <carlosmato1988@gmail.com>
2023-03-11 21:37:40 +0100

    up_mail_int

--- mail.py
+++ mail.py
@@ -37,8 +37,8 @@ while True:
         message = socket_rec.recv_string()
         mode, text, name = procesar_mensaje2(message)
 
-        if mode != 'whatsapp':
-            continue
+        #if mode != 'whatsapp':
+        #    continue
 
         print(f"Modo: {mode}")
         print(f"Texto: {text}")
@@ -46,8 +46,19 @@ while True:
 
         PATH = ChromeDriverManager().install()
         driver=webdriver.Chrome(PATH, options=options)
-        driver.get('https://web.whatsapp.com/')
+        driver.get('https://accounts.google.com/b/0/AddMailService')
 
+    except Exception as e:
+        logging.error(f"Ocurrió un error: {e}")
+        time.sleep(1)
+        continue
+
+
+
+
+
+
+'''
         wait = WebDriverWait(driver, 30)  # Wait for up to 10 seconds
 
         busca = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')))
@@ -57,35 +68,6 @@ while True:
 
         busca.click()
         busca.send_keys('')
-        time.sleep(1)
-        busca.send_keys(Keys.TAB, Keys.TAB)
-        time.sleep(0.5)
-        busca.send_keys(Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB)
 
-        txt = driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
-        txt.send_keys(text)
-        time.sleep(1)
-
-        ent = driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')
-        ent.click()
-        time.sleep(0.4)
-
-        driver.quit() 
-
-        continue
-
-    except Exception as e:
-        logging.error(f"Ocurrió un error: {e}")
-        time.sleep(1)
-        continue
     
-        
-
-
-
-
-
-
-
-
-
+'''
